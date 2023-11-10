4097cfe12482e7dfabbef7268f59bf95f00d0e67
Viktor <mail.victor.kostov@gmail.com>
2023-11-10 16:55:34 +0200

    Foo

--- hackv2/format.sh
+++ hackv2/format.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "Running hackv2/format.sh" && exit
 #
 # Copyright 2019 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
 #
